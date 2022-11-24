9c856308f6f32e1306f266d394a84868169952c5
pequito193 <vitorspequito@gmail.com>
2022-11-24 11:40:55 +0000

    Change routes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var continentsRouter = require('./routes/continents');
+var countriesRouter = require('./routes/countries');
 
 var app = express();
 
@@ -27,7 +28,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/continents', continentsRouter);
+app.use('/countries', countriesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/continents.js
+++ routes/continents.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('continents');
 });
+
+// router.get('/:id', function(req, res, next) {
+//   const continent = req.body.id;
+
+// })
 
 module.exports = router;
